66f09d271fe923950429d847e7aaee941cf07ffc
Jay <48680156+Ninjani@users.noreply.github.com>
2026-01-17 21:38:47 +0100

    typo fix

--- tea/convert.py
+++ tea/convert.py
@@ -76,9 +76,7 @@ def run_batch(
 ):
     device = next(tea.parameters()).device
     try:
-        spaced_seqs = [
-            " ".join(list(re.sub(r"[UZOBJ]", "X", seq.replace("-", ""))) for _, seq in sequences
-        ]
+        spaced_seqs = [" ".join(list(re.sub(r"[UZOBJ]", "X", seq.replace("-", "")))) for _, seq in sequences]
         batch = tokenizer.batch_encode_plus(
             spaced_seqs, add_special_tokens=True, padding="longest"
         )
